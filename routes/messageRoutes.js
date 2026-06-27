a470bcc78fe01f179b61957ce1f5f46e6beb3d76
Aqsa Ishtiaq <aqsaishtiaq19@gmail.com>
2026-06-28 00:36:27 +0500

    Fix message model import case sensitivity

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Message = require("../models/Message");
+const Message = require("../models/message");
 
 // POST message
 router.post("/", async (req, res) => {
